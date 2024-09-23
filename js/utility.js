d1d1f5d3464b0c8e10914814304ae215a189d51e
unknown <sikuahamed@gmail.com>
2024-09-23 20:25:15 +0600

    assignment 5 done

--- js/utility.js
+++ js/utility.js
@@ -8,7 +8,6 @@ function classes(id) {
   return document.getElementById(id).classList;
 }
 
-
 function modal(id) {
   if (id > 0 && !isNaN(id)) {
     document.getElementById(
@@ -20,7 +19,7 @@ function modal(id) {
         <h3 class="text-xl font-bold">Successfully</h3>
         <div class="modal-action mx-auto">
           <form method="dialog" class="mx-auto">
-            <button class="btn">Close confirmation</button>
+            <button class="btn bg-primaryColor">Close confirmation</button>
           </form>
         </div>
       </div>`;
@@ -29,31 +28,19 @@ function modal(id) {
       // "modalContainer"
       "my_modal_1"
     ).innerHTML = `
-    <div id="modalContainer" class="modal-box text-center">
-      <p>Insufficient Balance</p>
+    <div id="modalContainer" class="modal-box text-center space-y-5">
+      <p class="text-xl font-bold">Insufficient Balance</p>
       <form method="dialog" class="mx-auto">
-        <button class="btn">Close confirmation</button>
+        <button class="btn bg-primaryColor">Close confirmation</button>
       </form>
     </div>`;
   }
 }
 
 // Blog Btns function
-
-document.getElementById("blogbtn").addEventListener('click', function(){
-  window.location.href = 'blog.html';
-})
-
-// toggleButton.addEventListener("click", () => {
-//   if (toggleButton.textContent === "Blog") {
-//     toggleButton.textContent = "Home";
-//     window.location.href = 'home.html';
-//   } else {
-//     toggleButton.textContent = "Blog";
-//   }
-// });
-
-
+document.getElementById("blogbtn").addEventListener("click", function () {
+  window.location.href = "blog.html";
+});
 
 // Click History btn
 document.getElementById("historybtn").addEventListener("click", function () {
@@ -63,8 +50,6 @@ document.getElementById("historybtn").addEventListener("click", function () {
   classes("historybtn").remove("btn-outline");
   classes("donationBtn").remove("bg-primaryColor");
   classes("donationBtn").add("btn-outline");
-
-
 });
 
 // Click donation btn
@@ -91,6 +76,18 @@ document.getElementById("donateBtnOne").addEventListener("click", function () {
 
   modal(inputOneValue);
   document.getElementById("inputOne").value = "";
+
+  // historyList functions in divOne
+  const historyList = document.createElement("div");
+  const divOneTitle = document.getElementById("divOneTitle").innerText;
+  historyList.innerHTML = `<div class="border-2 border-gray-300 rounded-lg">
+    <div class="p-5 space-y-3">
+      <p class="text-gray-700 text-xl lg:text-3xl font-bold">${inputOneValue} Taka is Donated for ${divOneTitle}</p>
+      <p class="text-gray-700 text-sm lg:text-xl font-medium"> Date: ${new Date().toString()}</p>
+    </div>
+  </div>
+  `;
+  document.getElementById("historyContainer").appendChild(historyList);
 });
 
 // divTwo
@@ -107,6 +104,18 @@ document.getElementById("donateBtnTwo").addEventListener("click", function () {
 
   modal(inputTwoValue);
   document.getElementById("inputTwo").value = "";
+
+    // historyList functions in divTwo
+    const historyList = document.createElement("div");
+    const divTwoTitle = document.getElementById("divTwoTitle").innerText;
+    historyList.innerHTML = `<div class="border-2 border-gray-300 rounded-lg">
+      <div class="p-5 space-y-3">
+        <p class="text-gray-700 text-xl lg:text-3xl font-bold">${inputTwoValue} Taka is Donated for ${divTwoTitle}</p>
+        <p class="text-gray-700 text-sm lg:text-xl font-medium"> Date: ${new Date().toString()}</p>
+      </div>
+    </div>
+    `;
+    document.getElementById("historyContainer").appendChild(historyList);
 });
 
 //div Three
@@ -114,18 +123,29 @@ document
   .getElementById("donateBtnThree")
   .addEventListener("click", function () {
     const amounthreeValue = getInnerText("amountThree");
-    const inputthreeValue = getValue("inputThree");
-    if (inputthreeValue > 0 && !isNaN(inputthreeValue)) {
+    const inputThreeValue = getValue("inputThree");
+    if (inputThreeValue > 0 && !isNaN(inputThreeValue)) {
       document.getElementById("amountThree").innerText =
-        amounthreeValue + inputthreeValue;
+        amounthreeValue + inputThreeValue;
       const topCornerBtn = getInnerText("mainBalance");
       document.getElementById("mainBalance").innerText =
-        topCornerBtn - inputthreeValue;
+        topCornerBtn - inputThreeValue;
     }
 
-    modal(inputthreeValue);
+    modal(inputThreeValue);
     document.getElementById("inputThree").value = "";
-  });
 
+      // historyList functions in divThree
+      const historyList = document.createElement("div");
+      const divThreeTitle = document.getElementById("divThreeTitle").innerText;
+      historyList.innerHTML = `<div class="border-2 border-gray-300 rounded-lg">
+        <div class="p-5 space-y-3">
+          <p class="text-gray-700 text-xl lg:text-3xl font-bold">${inputThreeValue} Taka is Donated for ${divThreeTitle}</p>
+          <p class="text-gray-700 text-sm lg:text-xl font-medium"> Date: ${new Date().toString()}</p>
+        </div>
+      </div>
+      `;
+      document.getElementById("historyContainer").appendChild(historyList);
+  });
 
 
